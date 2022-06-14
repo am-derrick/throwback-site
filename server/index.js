cb1709ccb0e530395a2431c9b536bda44e3b465b
Derrick Ampire <ampire90@gmail.com>
2022-06-14 15:55:19 +0300

    Initial files added

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const username = encodeURIComponent("am_derrick");
+const password = encodeURIComponent("9%RBDpKznvmeU#Q");
+
+const CONNECTION_URL = `mongodb+srv://${username}:${password}@cluster0.zsbfk.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
